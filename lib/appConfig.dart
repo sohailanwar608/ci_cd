07270bb55b0acbc9edf1256a1102ef0a0ae3038c
sohail anwar <sohailanwar@sohails-MacBook-Pro.local>
2024-01-20 01:54:16 +0500

    changed build.gradle

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -3,5 +3,5 @@ class AppConfig
   String appName = "ali";
   String packageName = "com.example.ci_cd";
   String appIcon = "desk_icon.png";
-  String versionNumber = "1.1.0+1";
+  String versionNumber = "1.2.0+2";
 }
