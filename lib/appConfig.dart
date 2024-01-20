d91abfa1e3417e502da28688564601efc60c58c8
Sohail Anwar <99714436+sohailanwar608@users.noreply.github.com>
2024-01-20 23:33:37 +0500

    Update appConfig.dart

--- lib/appConfig.dart
+++ lib/appConfig.dart
@@ -1,6 +1,6 @@
 class AppConfig
 {
-  String appName = "ali";
+  String appName = "sohail";
   String packageName = "com.example.ci_cd";
   String appIcon = "desk_icon.png";
   String versionNumber = "1.2.0+2";
